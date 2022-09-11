084008fccdd6e0ae9901a370706fcd73b6c4d285
Dragon-Seeker <timekeeperguild@gmail.com>
2022-09-11 15:09:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Personality
+
+A Mod designed to add more Personal Elements to minecraft with Origin Support.
+
---
+++ src/main/java/io/wispforest/personality/Personality.java
@@ -0,0 +1,11 @@
+package io.wispforest.personality;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Personality implements ModInitializer {
+
+	@Override
+	public void onInitialize() {}
+}
