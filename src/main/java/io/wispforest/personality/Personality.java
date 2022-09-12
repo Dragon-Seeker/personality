5c0fe61ebef25c282ed8afb990ab4969213d83b0
minenash <minenash@protonmail.com>
2022-09-12 17:39:37 -0400

    Removed test code

--- src/main/java/io/wispforest/personality/Personality.java
+++ src/main/java/io/wispforest/personality/Personality.java
@@ -1,21 +1,12 @@
 package io.wispforest.personality;
 
-import io.wispforest.personality.storage.Character;
 import io.wispforest.personality.storage.CharacterManager;
 import net.fabricmc.api.ModInitializer;
-
-import java.util.UUID;
-
 
 public class Personality implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
 		CharacterManager.loadCharacterReference();
-		System.out.println("test");
-		for (int i = 0; i < 10; i++)
-			CharacterManager.playerToCharacters.put(UUID.randomUUID().toString(), UUID.randomUUID().toString());
-		System.out.println("test2");
-		CharacterManager.saveCharacterReference();
 	}
 }
